402cf612ec10569ecc7d0f2623bd98e605e76da3
margipandya <margipandya900@gmail.com>
2022-07-30 07:55:29 -0700

    my first commit

---
+++ 12.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	
+	
+	int i=5;
+	float f=10.6;
+	char c='A';
+	double d=19.50000;
+	
+	printf("int value %d\n",i);
+	printf("float value %f\n",f);
+	printf("char value %c\n",c);
+	printf("double value %if\n",d);
+}
